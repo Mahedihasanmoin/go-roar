79874fe019e60cbf697ba46b057ccc1e97edec24
mahedi <moindoom@gmail.com>
2019-07-09 13:46:13 +0600

    generate model by CLI functionality

--- cmd/model.go
+++ cmd/model.go
@@ -1,19 +1,25 @@
 package cmd
 
 import (
+	"fmt"
 	"github.com/spf13/cobra"
+	"io/ioutil"
 	"log"
+	"os"
+	"strings"
 )
-
+var modelName string
 // serveCmd represents the serve command
 
 var createModelCmd = &cobra.Command{
-	Use:   "model",
+	Use:   "make:model",
 	Short: "start http server with configured api",
 	Long:  `Starts a http server and serves the configured api`,
 	Run: func(cmd *cobra.Command, args []string) {
 		getModelName, _:= cmd.Flags().GetString("getModelName")
+		modelName = getModelName
 		log.Printf("Create model cmd init",getModelName)
+		genModel(getModelName)
 	},
 }
 
@@ -29,5 +35,70 @@ func init() {
 	// is called directly, e.g.:
 	// serveCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 	createModelCmd.Flags().StringP("getModelName", "n", "", "Set your model name")
+
+}
+
+func modelStr(modelName string)  string{
+	var model = fmt.Sprintf(`package model
+		import (
+		_ "github.com/go-sql-driver/mysql"
+		//"github.com/jinzhu/gorm"
+		_ "github.com/jinzhu/gorm/dialects/mysql"
+		//"go-roar/helper"
+		"net/http"
+		)
+		type %s struct {
+			gorm.Model
+		}
+		
+		func Store%sData(w http.ResponseWriter,item %s ) {
+		
+		}
+		
+		func GetAll%sData(w http.ResponseWriter){
+		
+		}
+		
+		func GetById%sData(w http.ResponseWriter,id int){
+		
+		}
+		
+		func Del%sData(w http.ResponseWriter,id int) {
+			
+		}
+		
+		func Update%sData(w http.ResponseWriter) {
+		
+		}
+		`,modelName,modelName,modelName,modelName,modelName,modelName,modelName)
+	return model
+}
+
+
+func genModel(getModelName string) {
+	formatModelName := strings.Replace(strings.Title(strings.Replace(getModelName, "_", " ", -1)), " ", "", -1)
+	path := fmt.Sprintf("model/%s.go",formatModelName)
+	_, err := os.Stat(path)
+	if err == nil {
+		log.Printf("model %s exists", formatModelName)
+	} else if os.IsNotExist(err) {
+		f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE, 0666)
+		if err != nil {
+			//handle error
+			log.Printf("model create %s error", err)
+		}
+
+		defer f.Close()
+
+	} else {
+		log.Printf("model %s stat error: %v", formatModelName, err)
+	}
+
+	modelSTR := modelStr(formatModelName)
+	if err := ioutil.WriteFile(path, []byte(modelSTR), 0644); err != nil {
+		log.Println(err)
+		return
+	}
+	fmt.Println("OK")
 
 }
